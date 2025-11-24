7aeb3a6e3714657901676eb6b7ddb273d0819eca
Sherwin Adonis M.  Vizcarra - II <sherwin7222@gmail.com>
2025-11-24 11:39:42 +0800

    Footer-Bar Added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Footer from './layouts/footer'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel='noopener'>
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel='noopener'>
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <div className="text-2xl">HELLO</div>
-        <div className="text-5xl">HELLO</div>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </main>
+      <Footer />
     </>
   )
 }
