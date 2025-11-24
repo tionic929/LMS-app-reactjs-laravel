85c7a89beedfeebad15ce350d14914c68db62b5f
Sherwin Adonis M.  Vizcarra - II <sherwin7222@gmail.com>
2025-11-24 14:38:45 +0800

    Added 4 Static CardCounters

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,47 +1,41 @@
-import viteLogo from '/vite.svg'
-import reactLogo from './assets/react.svg'
-import Sidebar from './layouts/sidebar'
-import Navbar from './layouts/navbar'
-import Footer from './layouts/footer'
+import { useState } from 'react'
 import './App.css'
+import Sidebar from './layouts/sidebar'
+import Footer from './layouts/footer'
+import Navbar from './layouts/navbar'
 
 function App() {
   return (
+    <>
     <div className="h-screen flex">
       <Sidebar />
-
-      <div className="flex-1 flex flex-col">
+        <div className="flex-1 flex flex-col">
         <Navbar />
+          <main className="flex-1 overflow-auto p-6">
+            <div className="max-w-4xl mx-auto">
 
-        <main className="flex-1 overflow-auto p-6">
-          <div className="max-w-6xl mx-auto">
-            <div className="flex gap-4 items-center mb-6">
-              <a href="https://vite.dev" target="_blank" rel="noopener">
-                <img src={viteLogo} className="logo" alt="Vite logo" />
-              </a>
-              <a href="https://react.dev" target="_blank" rel="noopener">
-                <img src={reactLogo} className="logo react" alt="React logo" />
-              </a>
-            </div>
+              <div className="mt-6">
+                <div className="text-2xl">HELLO</div>
+                <div className="text-5xl">HELLO</div>
+              </div>
 
-            <div className="text-2xl">HELLO</div>
-            <div className="text-5xl">HELLO</div>
+              <h1 className="mt-6">Vite + React</h1>
 
-            <h1 className="mt-6">Vite + React</h1>
-            <div className="card mt-4">
-              <p>
-                Edit <code>src/App.tsx</code> and save to test HMR
+              <div className="card mt-4">
+                <p>
+                  Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+              </div>
+
+              <p className="read-the-docs mt-4">
+                Click on the Vite and React logos to learn more
               </p>
             </div>
-            <p className="read-the-docs mt-4">
-              Click on the Vite and React logos to learn more
-            </p>
-          </div>
-        </main>
-
+          </main>
         <Footer />
+        </div>
       </div>
-    </div>
+      </>
   )
 }
 
