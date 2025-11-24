7737217fc9ee089212c2356cbbf20f4c22f4bdb3
Tigidodong <seasco24@gmail.com>
2025-11-24 11:47:41 +0800

    added the navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './layouts/navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Navbar />
       <div>
         <a href="https://vite.dev" target="_blank" rel='noopener'>
           <img src={viteLogo} className="logo" alt="Vite logo" />
