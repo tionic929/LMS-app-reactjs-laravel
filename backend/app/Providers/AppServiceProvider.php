f157785f998dd0973f8c0ff28e4a1fc5d5a3370c
P4NTH-3R <vincentpagaduan04@gmail.com>
2025-11-26 15:56:12 +0800

    with working announcement crud

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // No CSRF-exceptions here: use Sanctum + XSRF cookie flow for protected routes.
     }
 }
